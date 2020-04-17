b2e687b89db84f5a31dfc6b67242ed5c35170844
董鹏飞 <1270469522@qq.com>
2020-04-17 09:21:39 +0000

    用户话题列表

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
@@ -26,5 +27,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'introduction' => $faker->sentence(),
         'remember_token' => Str::random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
