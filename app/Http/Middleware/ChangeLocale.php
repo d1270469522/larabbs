be54693f91cd8e1946be0625d9fe599bffe2dd99
董鹏飞 <1270469522@qq.com>
2020-05-08 06:44:46 +0000

    话题单元测试

--- app/Http/Middleware/ChangeLocale.php
+++ app/Http/Middleware/ChangeLocale.php
@@ -9,7 +9,9 @@ class ChangeLocale
     public function handle($request, Closure $next)
     {
         $language = $request->header('accept-language');
+
         if ($language) {
+            $language = explode(',', $language)[0];
             \App::setLocale($language);
         }
 
